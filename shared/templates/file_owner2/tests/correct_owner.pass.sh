dc8e05066bdc37cb2d7786decd9dfac1927bcedf
Eric Berry <eric.berry@canonical.com>
2025-03-14 14:37:02 -0700

    Implement file_owner2 template

---
+++ shared/templates/file_owner2/tests/correct_owner.pass.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+{{% if PACKAGES %}}
+# packages = {{{PACKAGES}}}
+{{% endif %}}
+
+{{% set OWNERS=OWNER.split("|") %}}
+{{% if OWNERS|length > 1 %}}
+echo "Create specific tests for this rule because of mulitple owners"
+exit 1;
+{{% else %}}
+{{% for own in OWNERS %}}
+{{% for path in FILEPATH %}}
+{{% if path.endswith("/") %}}
+if [ ! -d {{{ path }}} ]; then
+    mkdir -p {{{ path }}}
+fi
+{{% if FILE_REGEX %}}
+echo "Create specific tests for this rule because of regex owner"
+exit 1;
+{{% elif RECURSIVE %}}
+find -L {{{ path }}} -type d -exec chown {{{ own }}} {} \;
+{{% else %}}
+chown {{{ own }}} {{{ path }}}
+{{% endif %}}
+{{% else %}}
+if [ ! -f {{{ path }}} ]; then
+    mkdir -p "$(dirname '{{{ path }}}')"
+    touch {{{ path }}}
+fi
+chown {{{ own }}} {{{ path }}}
+{{% endif %}}
+{{% endfor %}}
+{{% endfor %}}
+{{% endif %}}
---
+++ shared/templates/file_owner2/tests/incorrect_owner.fail.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+{{% if PACKAGES %}}
+# packages = {{{PACKAGES}}}
+{{% endif %}}
+
+useradd testuser_123
+
+{{%- if RECURSIVE %}}
+{{% set FIND_RECURSE_ARGS="" %}}
+{{%- else %}}
+{{% set FIND_RECURSE_ARGS="-maxdepth 1" %}}
+{{%- endif %}}
+
+{{% for path in FILEPATH %}}
+{{% if path.endswith("/") %}}
+if [ ! -d {{{ path }}} ]; then
+    mkdir -p {{{ path }}}
+fi
+{{% if FILE_REGEX %}}
+echo "Create specific tests for this rule because of regex owner"
+{{% elif RECURSIVE %}}
+find -L {{{ path }}} -type d -exec chown testuser_123 {} \;
+{{% else %}}
+chown testuser_123 {{{ path }}}
+{{% endif %}}
+{{% else %}}
+if [ ! -f {{{ path }}} ]; then
+    mkdir -p "$(dirname '{{{ path }}}')"
+    touch {{{ path }}}
+fi
+if [ -L {{{ path }}} ]; then
+    rm {{{ path }}}
+    touch {{{ path }}}
+fi
+chown testuser_123 {{{ path }}}
+{{% endif %}}
+{{% endfor %}}
